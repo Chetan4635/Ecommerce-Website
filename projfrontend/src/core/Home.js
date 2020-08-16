e126bc02a16793c655bfeb49c272bff5f3f6406b
sayannath <sayannath235@gmail.com>
2020-08-17 05:28:50 +0530

    Menu.js

---
+++ projfrontend/src/core/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles.css";
+import { API } from "../backend";
+import Base from "./Base";
+
+export default function Home() {
+  console.log("API IS", API);
+  return (
+    <Base title="Home Page">
+      <div className="row">
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+        <button className="btn btn-success">TEST</button>
+        <div className="col-4"></div>
+        <button className="btn btn-success">TEST</button>
+        <div className="col-4"></div>
+      </div>
+    </Base>
+  );
+}
